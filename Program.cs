58fc851e8bb5f2492ab76caec81605b1c44dfc06
Kuruzso <Vellera@kkszki.hu>
2024-09-24 14:00:58 +0200

    beiras

--- Program.cs
+++ Program.cs
@@ -40,13 +40,38 @@ namespace Auto
             conn.Connection.Close();
         
         }
+
+        public static void addNewCar() {
+        
+            conn.Connection.Open();
+
+            string brand, type,license;
+            int date;
+
+            Console.Write("Kérem az autó márkáját:");
+           brand = Console.ReadLine();
+            Console.Write("Kérem az típusát ");
+            type = Console.ReadLine();
+            Console.Write("Kérem az motorszámát ");
+            license = Console.ReadLine();
+            Console.Write("Kérem az típusát gyártási évét ");
+            date = int.Parse(Console.ReadLine());
+
+            string sql = $"INSERT INTO `cars`(`Brand`, `Type`, `License`, `Date`) VALUES ('{brand}','{type}','{license}',{date})";
+            MySqlCommand cmd = new MySqlCommand(sql,conn.Connection);
+            cmd.ExecuteNonQuery();
+
+            conn.Connection.Close();
+        
+        }
         static void Main(string[] args)
         {
             feltolt();
             foreach (var item in cars)
             {
-                Console.WriteLine($"Autó gyártója: {item.Brand},motorszáma");
+                Console.WriteLine($"Autó gyártója: {item.Brand},motorszáma: {item.License}");
             }
+            addNewCar();
             Console.WriteLine();
             Console.ReadLine();
         }
